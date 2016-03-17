7d8e0884f9b5f12d3892468cfe5cb5eb45e21282
Romain Guillemot <contact@romainguillemot.com>
2016-03-17 13:39:26 +0100

    Replaced macros for version numbers with constant variables + Documented sb-global

--- src/sb-global/sb-global.h
+++ src/sb-global/sb-global.h
@@ -15,22 +15,65 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with Softbloks.  If not, see <http://www.gnu.org/licenses/>.
 */
+/*! \file sb-global.h */
 #ifndef SB_GLOBAL_H
 #define SB_GLOBAL_H
 
-// version macros
+// version numbers
 
 #include <sb-global/sb-version.h>
 
-#define SB_VERSION_(major_, minor_)\
-    ((major_<<8)|(minor_))
+namespace sb
+{
 
-#define SB_VERSION SB_VERSION_(SB_VERSION_MAJOR, SB_VERSION_MINOR)
+/// Turns \a major_, \a minor_ and \a patch_ into a version number of the form
+/// 0xMMNNPP where MM, NN and PP represent respectively the major, the minor
+/// and the patch. This can be compared with another similarly formed version
+/// number.
+///
+/// \sa VERSION and BUILD.
+inline
+int
+version_check
+(
+    int major_,
+    int minor_,
+    int patch_ = 0
+)
+{
+    return (
+        (major_ << 16)|
+        (minor_ <<  8)|
+        (patch_)
+    );
+}
 
-#define SB_BUILD_(year_, month_, day_)\
-    ((year_<<16)|(month_<<8)|(day_))
+/// Constant symbolizing Softbloks's version number.
+///
+/// The value is of the form 0xMMNN00 where MM and NN represent respectively
+/// the major and the minor.
+///
+/// \sa version_check(), VERSION_MAJOR, VERSION_MINOR and BUILD.
+const int VERSION = sb::version_check(
+    sb::VERSION_MAJOR,
+    sb::VERSION_MINOR
+);
 
-#define SB_BUILD SB_BUILD_(SB_BUILD_YEAR, SB_BUILD_MONTH, SB_BUILD_DAY)
+/// Constant symbolizing Softbloks's build number.
+///
+/// The value is of the form 0xYYMMDD where YY, MM and DD represent
+/// respectively the year, the month and the day.
+///
+/// \sa version_check(), BUILD_YEAR, BUILD_MONTH, BUILD_DAY and VERSION.
+const int BUILD = sb::version_check(
+    sb::BUILD_YEAR,
+    sb::BUILD_MONTH,
+    sb::BUILD_DAY
+);
+
+}
+
+/// \cond INTERNAL
 
 // os detection, see:
 // http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system
@@ -45,7 +88,7 @@ along with Softbloks.  If not, see <http://www.gnu.org/licenses/>.
 #   error "Softbloks doesn't support this OS for now: ask for it"
 #endif
 
-// attribute definition
+// visibility support
 
 #if defined(_MSC_VER)
 #   define SB_DECL_EXPORT       __declspec(dllexport)
@@ -71,6 +114,26 @@ along with Softbloks.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 #ifndef SB_DECL_HIDDEN
 #   define SB_DECL_HIDDEN
+#endif
+
+/// \endcond
+
+#ifdef DOXYGEN
+
+/// Defined on all supported versions of systems distributed by Apple.
+#define SB_OS_MAC
+/// Defined on all supported versions of Windows.
+#define SB_OS_WIN
+/// Defined on all supported versions of Linux.
+#define SB_OS_LINUX
+
+/// This macro declares a symbol to be an export from a shared library.
+#define SB_DECL_EXPORT
+/// This macro declares a symbol to be an import from a shared library.
+#define SB_DECL_IMPORT
+/// This macro declares a symbol to be hidden in a shared library.
+#define SB_DECL_HIDDEN
+
 #endif
 
 #endif // SB_GLOBAL_H
